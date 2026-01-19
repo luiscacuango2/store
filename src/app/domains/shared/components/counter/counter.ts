447e7a65114ba306c22bf8c6c7024ce200ca227d
Luis Cacuango <manuel.cacuango@est.umet.edu.ec>
2026-01-19 17:09:42 -0500

    feat: Curso de Angular: Creación de Aplicaciones Web Ciclo de Vida de Componentes en Angular

---
+++ src/app/domains/shared/components/counter/counter.ts
@@ -0,0 +1,41 @@
+import { Component, Input, SimpleChange } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.html',
+  styleUrl: './counter.css',
+})
+export class Counter {
+  @Input({required: true}) duration = 0;
+  @Input({required: true}) message = '';
+
+  constructor() {
+    // NO ASYNC AQUI
+    // Antes render
+    console.log('constructor');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnChanges() {
+    // NO ASYNC AQUI
+    // Antes y durante render
+    console.log('ngOnChanges');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnInit(changes: SimpleChange) {
+    // NO ASYNC AQUI
+    // Antes render
+    console.log('ngOnInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngAfterViewInit() {
+    // AQUI SI ASYNC
+    // Despues render
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+}
